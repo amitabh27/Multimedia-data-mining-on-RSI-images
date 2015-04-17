6e9e9475188f3bf400b8c2545e27130a18b60f81
Amitabh tiwari <amitabh.tiwari27@gmail.com>
2015-04-17 15:26:51 +0530

    Create parm5.c

---
+++ parm5.c
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+class parm5
+{
+
+static void func1()throws Exception 
+{
+BufferedImage  image,image2;
+int width;
+int height;
+
+File input = new File("generated_image1.jpg");
+image = ImageIO.read(input);
+width = image.getWidth();
+height = image.getHeight();
+         
+int i,j;  
+int red,green,blue;   
+for(i=0; i<height; i++)
+{
+for(j=0; j<width; j++)
+{
+            
+Color c = new Color(image.getRGB(j, i));
+red=(int)(c.getRed() * 0.299);
+green=(int)(c.getGreen() * 0.587);
+blue=(int)(c.getBlue() *0.114);
+Color newColor = new Color(red+green+blue,red+green+blue,red+green+blue);
+image.setRGB(j,i,newColor.getRGB());
+}
+}
+File ouptut = new File("generated_image1.jpg");
+ImageIO.write(image, "jpg", ouptut);
+
+      
+}
+
+
+   
+   public static void main(String args[]) throws Exception 
+   {
+     func1();
+   }
+}
